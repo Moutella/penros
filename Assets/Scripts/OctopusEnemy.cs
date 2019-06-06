6e5234bfd9e4851e1d9bfc20c7c510723ff19a99
Moutella <joaogabrielmsc@gmail.com>
2019-06-05 21:36:28 -0300

    OctopusEnemy herda Enemy e novo tile

--- Assets/Scripts/OctopusEnemy.cs
+++ Assets/Scripts/OctopusEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OctopusEnemy : MonoBehaviour
+public class OctopusEnemy : Enemy
 {
     // Start is called before the first frame update
     void Start()
