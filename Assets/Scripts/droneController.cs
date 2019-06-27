601b8ac3fbf3d3b6843e455b2b6899478dfb5eac
Moutella <joaogabrielmsc@gmail.com>
2019-06-27 13:09:26 -0300

    Drone animado com float corretamente

--- Assets/Scripts/droneController.cs
+++ Assets/Scripts/droneController.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class droneController : MonoBehaviour
 {
-    public Transform currentDesiredPos;
+    public Vector3 currentDesiredPos;
     public float droneVelo;
+    public List<Transform> posicoesDroneNoMundo;
+    public Transform playerFollowPos;
+    private int currentPosIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentPosIndex = 0;
+        currentDesiredPos = posicoesDroneNoMundo[0].position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, currentDesiredPos.position, droneVelo);
+        transform.position = Vector3.Lerp(transform.position, currentDesiredPos, droneVelo);
+    }
+
+    void GoToNextDesiredSpot()
+    {
+        currentDesiredPos = posicoesDroneNoMundo[currentPosIndex].position;
+    }
+    void ChangeToPlayerPos()
+    {
+        currentDesiredPos = playerFollowPos.position;
+    }
+    void goToSpecificPos(Transform objeto)
+    {
+        currentDesiredPos = objeto.position;
     }
 }
