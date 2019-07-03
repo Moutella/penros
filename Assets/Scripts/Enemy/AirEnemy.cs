f45985bf4f605d0cdcb33e099f7a72ab0c04d852
Arch <33100644+mttorres@users.noreply.github.com>
2019-07-03 02:02:03 -0300

    Adicionando comportamento generico de inimigo (falta alguns ajustes)

--- Assets/Scripts/Enemy/AirEnemy.cs
+++ Assets/Scripts/Enemy/AirEnemy.cs
@@ -5,23 +5,63 @@ using UnityEngine;
 public class AirEnemy : Enemy
 {
     // Start is called before the first frame update
+   
+    
+    /*
     void Start()
     {
-        
+        //rb = GetComponent<Rigidbody2D>();
     }
+    */
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ModeUpdate();
+        //EscolheAcao();
+        acaoescolhida = 2;
+        if(acaoescolhida == 1 && vendo){
+        	DirectTackle();
+        }
+        if(acaoescolhida == 2 && attackMode){
+        	Debug.Log("ei");
+        	Shoot();
+        }
+        /*
+        else{
+        	if(acaoescolhida == 1){
+        		acaoescolhida == 
+        	}
+        }
+        */
     }
+
+    void FixedUpdate(){
+    	ModeUpdate(); 
+    }
+
     // tentando fazer ele colidir com a TRAIL!
     void OnTriggerEnter2D(Collider2D other){
-    	Debug.Log(other.name);
-    	
+    	if(other.name.Equals("Player")){
+    		// ativa efeito (vi o jogador)
+    		SwitchMode();
+    		Debug.Log("TO TE VENDO FILHO DA PUTA");
+    	}	
     }
 
     void OnTriggerStay2D(Collider2D other){
-    	Debug.Log(other.name);
+    	if(other.name.Equals("Player")){
+    		//vendo = true;
+    		StayMode();
+    	}
     }
+
+    void OnTriggerExit2D(Collider2D other){
+    	if(other.name.Equals("Player")){
+    		vendo = false;
+    		
+    	}
+    }
+
+    // move (um movimento padrão que ele fica fazendo enquanto não ataca) (ele eventualmente RANDOM pode fazer esse movimento também mesmo em modo de ataque)
 }
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,8 +7,20 @@ public class Enemy : MonoBehaviour
     // script "padrão" de inimigos note que se formos criar outros tipos de inimigos os scripts deles
     // devem herdar esse!
 
-    public int vida = 100;
+    // componentes
+    private Rigidbody2D rb;
+    public GameObject enemybulletprefab;
     public GameObject morreueffect;
+    //variaveis
+    public int vida = 100;
+    public bool attackMode = false;
+    public bool vendo = false;
+    public float modeDuration = 60f;
+    public float moveSpeed = 20f;
+    public float shootSpeed = 20f;
+    public int shootCaden;
+    //private enum Acao{ padrao, movatac, tiro };
+    public  int  acaoescolhida;
 
     public void Dano(int dano){
         vida -= dano;
@@ -18,9 +30,84 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    void Die(){
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected void Die(){
         //Instantiate(morreueffect,transform.position, Quaternion.identity);
         Destroy(gameObject);
-    } 
+    }
+
+    protected void ModeUpdate(){
+        if(attackMode){
+            modeDuration = modeDuration - 0.1f;
+            if(modeDuration <= 0){
+                SwitchMode();
+                Debug.Log("tO DE BOA");
+            }
+        }
+    }
+    protected void SwitchMode(){
+        attackMode = !attackMode;
+        //vendo = !vendo;
+        modeDuration = 60f;
+    }
+
+    protected void StayMode(){
+        attackMode = true;
+        vendo = true;
+        modeDuration = 60f;
+    }
+
+    protected void EscolheAcao(){
+        if(attackMode){
+            int r = Random.Range(0, 3);
+            acaoescolhida = r;
+        }
+        else{
+            acaoescolhida = 0;
+        }
+        
+    }
+
+    // retorna vetor em direção ao jogador
+    protected Vector2 PersegueJogador(){
+        GameObject jogador = GameObject.Find("Player");
+        if(jogador != null){
+            Vector3 pe = transform.position;
+            Vector3 pp = jogador.transform.position;
+            rb.velocity = Vector2.zero;
+            Vector2 dir = new Vector2(pe.x-pp.x,pe.y-pp.y).normalized*(-1);
+            return dir;
+        }
+        return Vector2.zero;
+    }
+
+    // basicamente avança contra o jogador (é um dos ataques sorteados caso o jogador entre no "campo de visao dele")
+    protected void DirectTackle(){
+        Vector2 dir = PersegueJogador();
+        if(dir != Vector2.zero){
+            rb.velocity = dir*(moveSpeed);
+        }    
+            
+    }
+        
+    
+
+    // atira no jogador (é um dos ataques sorteados caso o jogador entre no "campo de visão dele") (o que muda é a cadencia de tiro de cada um deles)
+    protected void Shoot(){
+        Vector2 dir = PersegueJogador();
+        if(dir != Vector2.zero){
+            Debug.Log(dir);
+            //ativa efeitos
+            for(int i =0; i < shootCaden; i++){
+                GameObject tiroinimigo = Instantiate(enemybulletprefab,transform.position,transform.rotation) as GameObject;
+                tiroinimigo.GetComponent<Rigidbody2D>().velocity = dir*shootSpeed;
+            }        
+        }     
+    }  
     
 }
+
+
